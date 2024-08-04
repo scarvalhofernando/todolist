ec90a9ccb57ac36b454e168984ff0b163b1f2e5b
scarvalhofernando <scarvalho.fernando@outlook.com>
2024-08-04 15:33:25 -0300

    Iniciando projeto de lista de tarefas

---
+++ src/main/java/com/example/todolist/entity/Todo.java
@@ -0,0 +1,56 @@
+package com.example.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private String realizado;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(String realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
---
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -0,0 +1,40 @@
+package com.example.todolist.service;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioidade").descending().and(
+                Sort.by("nome").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
